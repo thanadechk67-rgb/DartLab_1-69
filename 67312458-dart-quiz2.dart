a786d14d1990aa2874dbd4ece5cd3a215010336e
thanadechk67-rgb <thanadechk67@nu.ac.th>
2026-07-15 11:51:53 +0700

    Add files via upload

---
+++ 67312458-dart-quiz2.dart
@@ -0,0 +1,47 @@
+abstract class MenuItem {
+	final String name;
+	final double basePrice;
+
+	MenuItem(this.name, this.basePrice);
+
+	double price();
+
+	void show() {
+		final p = price();
+		print('$name - ${p.toStringAsFixed(2)} บาท');
+  	print('ยอดรวมทั้งสิ้น - ${p.toStringAsFixed(2)} บาท');
+    print('---');
+	}
+}
+
+class FoodItem extends MenuItem {
+	final double extra;
+	FoodItem(String name, double basePrice, {this.extra = 0}) : super(name, basePrice);
+
+	@override
+	double price() => basePrice + extra;
+}
+
+void main() {
+	final sushi = FoodItem('ซูชิ', 120.0, extra: 10.5);
+	final salad = FoodItem('สลัด', 85.0);
+
+	sushi.show();
+	salad.show();
+}
+
+class drink extends MenuItem {
+double toppings;
+drink(String name, double basePrice, {this.toppings = 0}) : super(name, basePrice);
+
+@override
+double price() =>  basePrice + (10 * toppings)
+;
+  
+}
+
+class food extends MenuItem {
+
+
+
+}
