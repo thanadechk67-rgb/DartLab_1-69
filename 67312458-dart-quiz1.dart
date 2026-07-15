88e1a7702e776f3fd983bb5852904af5ee4b3bed
thanadechk67-rgb <thanadechk67@nu.ac.th>
2026-07-15 11:48:39 +0700

    Add files via upload

---
+++ 67312458-dart-quiz1.dart
@@ -0,0 +1,24 @@
+void main() {
+double vatRate = 0.07;
+String shopName = "Dart cafe";
+print ('อัตราภาษี : ${vatRate}');
+print('ร้าน : ${shopName}');
+print("---");
+
+
+
+List<String> categories  = ["cake","smooty","fruit"];
+var bath = {
+    "cake" : 40,
+    "smooty" : 50,
+    "fruit" : 100,
+
+};
+print('หมวดหมู่ : ${categories}');
+print('จำนวนทั้งหมด : ${categories.length}');
+print(bath ['cake']);
+print("---");
+
+
+
+}
